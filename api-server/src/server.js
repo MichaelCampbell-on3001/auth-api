43bc658d1ce472fc37f3a56df50fd4c96473ef37
MichaelCampbell2020 <cam3000@gmail.com>
2022-03-30 16:37:59 -0700

    setup complete

--- api-server/src/server.js
+++ api-server/src/server.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const express = require('express');
+// const express = require('express');
 
-const notFoundHandler = require('./error-handlers/404.js');
-const errorHandler = require('./error-handlers/500.js');
-const logger = require('./middleware/logger.js');
+// const notFoundHandler = require('./error-handlers/404.js');
+// const errorHandler = require('./error-handlers/500.js');
+// const logger = require('./middleware/logger.js');
 
-const v1Routes = require('./routes/v1.js');
+// const v1Routes = require('./routes/v1.js');
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(logger);
+// app.use(logger);
 
-app.use('/api/v1', v1Routes);
+// app.use('/api/v1', v1Routes);
 
-app.use('*', notFoundHandler);
-app.use(errorHandler);
+// app.use('*', notFoundHandler);
+// app.use(errorHandler);
 
 module.exports = {
   server: app,
