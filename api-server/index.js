a39628fd26b05eedc4df644a37b0df1557b8108a
MichaelCampbell2020 <cam3000@gmail.com>
2022-03-30 17:04:58 -0700

    index and server merged from two sets of starter code

--- api-server/index.js
+++ api-server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const { db } = require('./src/models');
-const server = require('./src/server.js');
+// const { db } = require('./src/models');
+// const server = require('./src/server.js');
 
-db.sync().then(() => {
-  server.start(3000);
-});
+// db.sync().then(() => {
+//   server.start(3000);
+// });
